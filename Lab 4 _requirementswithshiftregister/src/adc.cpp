65f4103c330e78288234c0b8ec5f05d61e87d2c0
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2025-04-19 02:34:10 +0700

    Update adc.cpp

--- Lab 4 _requirementswithshiftregister/src/adc.cpp
+++ Lab 4 _requirementswithshiftregister/src/adc.cpp
@@ -1,10 +1,3 @@
-//----------------------------------------------------------------------//
-// Author:  Hafiz, Sofia, Fadzar, Johan, Vincent       
-// Net ID:   muhammadhafiz, fadzarar, Vliem, putriyuandari, johaniswara       
-// Date:     08 April 2025      
-// Assignment: Lab 4 
-//----------------------------------------------------------------------//
-
 #include "adc.h"
 #include <avr/io.h>
 
