aa4c021074f5efb47a0300a08e98fabd040e6e7d
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2025-04-19 02:35:09 +0700

    Update pwm.h

--- Lab 4 _requirementswithshiftregister/src/pwm.h
+++ Lab 4 _requirementswithshiftregister/src/pwm.h
@@ -1,10 +1,3 @@
-//----------------------------------------------------------------------//
-// Author:  Hafiz, Sofia, Fadzar, Johan, Vincent       
-// Net ID:   muhammadhafiz, fadzarar, Vliem, putriyuandari, johaniswara      
-// Date:     08 April 2025      
-// Assignment: Lab 4 
-//----------------------------------------------------------------------//
-
 #ifndef PWM_H
 #define PWM_H
 
