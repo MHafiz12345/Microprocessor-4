daa847dbe56948e4ef5cfb0a6c09b823e74dbc6b
MHafiz12345 <147223828+MHafiz12345@users.noreply.github.com>
2025-04-19 02:34:35 +0700

    Update main.cpp

--- Lab 4 _requirementswithshiftregister/src/main.cpp
+++ Lab 4 _requirementswithshiftregister/src/main.cpp
@@ -1,10 +1,3 @@
-//----------------------------------------------------------------------//
-// Author:  Hafiz, Sofia, Fadzar, Johan, Vincent       
-// Net ID:   muhammadhafiz, fadzarar, Vliem, putriyuandari, johaniswara        
-// Date:     08 April 2025      
-// Assignment: Lab 4
-//----------------------------------------------------------------------//
-
 #include <avr/io.h>         
 #include <avr/interrupt.h>  
 #include "pwm.h"            
